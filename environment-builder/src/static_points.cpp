0ba5273d2989047253b93851a44350f92efd7661
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2019-03-02 18:59:20 +0100

    Package 'environment-builder' added, with 2 nodes: dynamic_objects and static_points for selecting dynamic/static obstacles from an occupancy grid

---
+++ environment-builder/src/static_points.cpp
@@ -0,0 +1,26 @@
+#include <babocar-core/types.hpp>
+#include "ros/ros.h"
+#include "nav_msgs/OccupancyGrid.h"
+
+void occupancyGridCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
+{
+    ROS_INFO("map size: %d x %d", msg->info.width, msg->info.height);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "environment_builder__static_points");
+    ros::NodeHandle n;
+    ros::Rate rate(1);
+    ros::Subscriber sub = n.subscribe("map", 1, occupancyGridCallback);
+
+    while(ros::ok()) {
+        ROS_INFO("Running");
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    ROS_INFO("Program ended.");
+
+    return 0;
+}
